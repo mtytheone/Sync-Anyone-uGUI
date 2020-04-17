a00d343e2bc16afcfbfe757b93884598eeb05f94
hatuxes <hatuxes@gmail.com>
2020-04-17 21:36:06 +0900

    [Create] Initial Commit.

---
+++ Assets/SyncAnyoneUI/Scripts/Dropdown_Anyone.cs
@@ -0,0 +1,51 @@
+#region License
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/* MIT License                                                                                                                                                                                                                                                                                                                                                                                                                                                                  */
+/*                                                                                                                                                                                                                                                                                                                                                                                                                                                                              */
+/* Copyright (c) 2000 hatuxes                                                                                                                                                                                                                                                                                                                                                                                                                                                   */
+/*                                                                                                                                                                                                                                                                                                                                                                                                                                                                              */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:                             */
+/*                                                                                                                                                                                                                                                                                                                                                                                                                                                                              */
+/* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.                                                                                                                                                                                                                                                                                                                                               */
+/*                                                                                                                                                                                                                                                                                                                                                                                                                                                                              */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+#endregion
+
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Dropdown_Anyone : UdonSharpBehaviour
+{
+    Dropdown _dropdown;
+    [SerializeField] Text _numText;
+    [SerializeField] Text _contentText;
+
+    [UdonSynced]
+    int _value;
+
+    void Start()
+    {
+        _dropdown = this.gameObject.GetComponent<Dropdown>();
+    }
+
+    void Update()
+    {
+        _dropdown.value = _value;
+        _numText.text = _value.ToString();
+        _contentText.text = _dropdown.captionText.text;
+    }
+
+    public void SetValue()
+    {
+        _value = _dropdown.value;
+    }
+
+    public void ChangerOwner()
+    {
+        if (!Networking.IsOwner(Networking.LocalPlayer, this.gameObject)) Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Sync Anyone uGUI
+
+[![License:MIT](https://img.shields.io/badge/License-MIT-819FF7.svg)](https://choosealicense.com/licenses/mit/)
+
+## About - 概要
+これはU#の解説記事のために作ったサンプルプロジェクトです。
+解説記事自体は[こちら](https://hatuxes.hatenablog.jp/entry/2020/04/17/182256)になります。
+
+VRCSDK3及びUdonSharpをインストールしてご使用ください。
+
+## License - ライセンス
+このプロジェクト及びソースファイルは **MIT** ライセンスに乗っ取ります。
+
+- [ライセンス本文](https://github.com/mtytheone/Sync-Anyone-uGUI/blob/master/LICENSE.md)
